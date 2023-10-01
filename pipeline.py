6b242ca50744f9e30097b2d96dfb923ad08f0ecd
Joao Pinheiro <jogp10@gmail.com>
2023-10-01 15:25:47 +0100

    readable uris

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,27 @@
 import json
 import csv
 import os
+import wikipedia
+import urllib.parse
+
+def transform_uri_to_readable(uri):
+    try:
+        return urllib.parse.unquote(uri)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return uri
+
+def transform_uris_to_readable(data, column):
+    try:
+        for record in data:
+            if record.get(column):
+                record[column] = transform_uri_to_readable(record[column])
+        return data
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return []
+
 
 def merge_json_files(file1_path, file2_path, id_column_name='id', output_path='merged_data.json'):
     try:
@@ -56,6 +77,17 @@ def get_unique_keys_from_json(json_file):
 def write_ordered_row(csv_writer, data, order):
     row = [data.get(key, '') for key in order]
     csv_writer.writerow(row)
+
+    
+def save_to_json(data, output_path):
+    try:
+        with open(output_path, 'w') as file:
+            json.dump(data, file, indent=4)
+        
+        print(f"Successfully saved data to {output_path}")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
     
 def json_to_csv(json_file, csv_file):
     try:
@@ -106,6 +138,66 @@ def convert_files(input_file, output_file):
     else:
         print("Unsupported file conversion: Please provide valid input and output file extensions.")
 
+def get_json_ids(json_files, id_column):
+    try:
+        ids = set()
+        for json_file in json_files:
+            with open(json_file, 'r') as file:
+                data = json.load(file)
+            
+            for record in data:
+                ids.add(record.get(id_column))
+        
+        return ids
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return set()
+    
+def get_json_column_values(json_file, column_name, id_column='', id_values=''):
+    try:
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+        
+        values = {}
+        for record in data:
+            if id_values == '' or record.get(id_column) in id_values:
+                values[record.get(id_column)] = record.get(column_name)
+        
+        return values
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return []    
+
+def get_wikipedia_summary(wikipedia_url):
+    try:
+        page = wikipedia.page(title=wikipedia_url.split('/')[-1])
+        return page.summary
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return ''
+    
+def get_wikipedia_summaries(event_data, wikipedia_column='article_en'):
+    size = len(event_data)
+    for idx, event in enumerate(event_data):
+        wikipedia_url = event.get(wikipedia_column)
+        if wikipedia_url:
+            summary = get_wikipedia_summary(wikipedia_url)
+            event["wikipedia_summary"] = summary
+            #print(f"{idx}/{size}: Retrieved summary for {wikipedia_url}")
+    return event_data
+
+def json_string_to_list(data, column_name, sep=';'):
+    try:
+        for record in data:
+            if record.get(column_name):
+                record[column_name] = record[column_name].split(sep)
+        return data
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return []
 
 
 
@@ -116,9 +208,29 @@ if __name__ == '__main__':
     file3_path = 'wikidata/eventwphoto.json'
     output_path = 'wikidata/merged_data.json'
     output_path_csv = 'wikidata/merged_data.csv'
+    wikipediafile_path = 'wikidata/wikipedia.json'
+    output_file = "data.json"
+    output_file_csv = "data.csv"
 
+    # Join the data from the two JSON files based on the 'event' column
     merged_data = merge_json_files(file1_path, file2_path, 'event', output_path)
     merged_data = merge_json_files(output_path, file3_path, 'event', output_path)
 
-    convert_files(output_path, output_path_csv)  # Calls json_to_csv
-    print("Merged data saved to 'merged_data.json'")
+    # Transform the participants column from a string to a list
+    merged_data = json_string_to_list(merged_data, 'participants')
+
+    # Transform the wikipedia_urls column to a readable format
+    merged_data = transform_uris_to_readable(merged_data, 'article_en')
+
+    # Save the merged data to a JSON file
+    save_to_json(merged_data, output_path)
+
+    # Add the wikipedia summaries to the merged data
+    merged_data = get_wikipedia_summaries(merged_data)
+
+    # Save the merged data to a JSON file
+    save_to_json(merged_data, wikipediafile_path)
+
+    convert_files(wikipediafile_path, output_file_csv)
+    convert_files(output_path, output_file_csv)
+    print("Merged data saved to data.json and data.csv")
