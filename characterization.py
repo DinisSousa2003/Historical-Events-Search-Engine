919eed1e5c91fafced388d810cafa63e4f50344c
João Matos <jcarvalhaismatos@gmail.com>
2023-10-07 16:00:43 +0100

    display a bar graph of the 20 most referenced entities per entity label

--- characterization.py
+++ characterization.py
@@ -53,20 +53,45 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 relevant_labels = {'PERSON', 'NORP', 'FAC', 'ORG', 'GPE', 'LOC', 'EVENT'}
-relevant_entities = {}
-for label in relevant_labels:
-    relevant_entities[label] = {}
+# relevant_entities = {}
+# for label in relevant_labels:
+#     relevant_entities[label] = {}
 
-for event in data:
-    doc = nlp(event['summary'])
-    for ent in doc.ents:
-        if ent.label_ in relevant_labels:
-            if ent.text in relevant_entities[ent.label_]:
-                relevant_entities[ent.label_][ent.text] += 1
-            else:
-                relevant_entities[ent.label_][ent.text] = 1
+# for event in data:
+#     doc = nlp(event['summary'])
+#     for ent in doc.ents:
+#         if ent.label_ in relevant_labels:
+#             if ent.text in relevant_entities[ent.label_]:
+#                 relevant_entities[ent.label_][ent.text] += 1
+#             else:
+#                 relevant_entities[ent.label_][ent.text] = 1
 
 # store in entities.json
-with open('outputs/entities.json', 'w', encoding='utf-8') as file:
-    json.dump(relevant_entities, file, ensure_ascii=False, indent=4)
+# with open('outputs/entities.json', 'w', encoding='utf-8') as file:
+#     json.dump(relevant_entities, file, ensure_ascii=False, indent=4)
+#
 
+# open entities.json and find the most relevant entities
+with open('outputs/entities.json', encoding='utf-8') as file:
+    entities = json.load(file)
+# entities is a dicionary of dictionaries
+# each dictionary contains the entities of a specific label
+# each entity has a count of how many times it appears in the summaries
+# sort by count and filter by count > 10
+for label in entities:
+    entities[label] = {k: v for k, v in sorted(entities[label].items(), key=lambda item: item[1], reverse=True)}
+    entities[label] = {k: v for k, v in entities[label].items() if v > 10}
+
+print(entities)
+
+# for each label show a plot of the first 20 entities
+for label in entities:
+    plt.bar(list(entities[label].keys())[:20], list(entities[label].values())[:20], color='g')
+    plt.xlabel('Entity')
+
+    # make the bars wider and separeted
+    plt.rcParams['figure.figsize'] = [30, 12]
+    plt.rcParams['figure.dpi'] = 60
+    plt.ylabel('Number of conflicts')
+    plt.title(label)
+    plt.show()
