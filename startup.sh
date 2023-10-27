0e8150d7e8d33d69cf6f11a28bf803e6e6e3249f
João Matos <jcarvalhaismatos@gmail.com>
2023-10-27 11:59:21 +0100

    fixed typos in info needs and updated startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # This script expects a container started with the following command.
-# docker run -p 8983:8983 --name pri_44 -v ${PWD}:/data -d solr:9.3 solr-precreate historicalEvents
+ #docker run -p 8983:8983 --name pri_44 -v ${PWD}:/data -d solr:9.3 solr-precreate conflicts
 
 # Schema definition via API
 curl -X POST -H 'Content-type:application/json' \
     --data-binary "@./schema.json" \
-    http://localhost:8983/solr/historicalEvents/schema
+    http://localhost:8983/solr/conflicts/schema
+
+curl -X POST -H 'Content-type:application/json' --data-binary "@./outputs/data.json" http://localhost:8983/solr/conflicts/update?commit=true
 
 # Populate collection using mapped path inside container.
-docker exec -it pri_44 bin/post -c historicalEvents ./outputs/data.json
+docker exec -it pri_44 bin/post -c conflicts ./outputs/data.json
