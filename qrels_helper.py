467ff1f1431a9500ff6a6b51b9791592b2d31e1d
João Matos <jcarvalhaismatos@gmail.com>
2023-11-11 20:55:29 +0000

    economic_consequences_revolutions.txt qrel

--- qrels_helper.py
+++ qrels_helper.py
@@ -108,9 +108,63 @@ def portuguese_as_allies():
         print("\n")
 
 
+def revolutions_economic_consequences():
+
+    with open('outputs/data.json', encoding='utf-8') as f:
+        data = json.load(f)
+
+    search_terms = ['revolution']
+    extra_search_terms = ['economic', 'economy', 'rich', 'poor', 'wealth', 'poverty',
+                            'prosperity', 'depression', 'recession', 'inflation', 'deflation', 'debt',
+                            'bankrupt', 'market','trade', 'trading', 'commerce', 'commercial', 'merch',
+                            'industrial', 'industry', 'industries', 'industrialization', 'industrialisation']
+
+    results = []
+    for event in data:
+        if any(term in event["summary"].lower() or term in event["label"].lower() or 'part of' in event and term in ' '.join(event["part of"]).lower() for term in search_terms):
+            if any(term in event["summary"].lower() or term in event["label"].lower() or 'part of' in event and term in ' '.join(event["part of"]).lower() for term in extra_search_terms):
+                results.append(event)
+
+
+    results.sort(key=lambda x: x["date"])
+
+    output_file = open('outputs/economic_consequences_revolutions_.txt', 'w', encoding='utf-8')
+    outputs = []
+
+
+
+    for event in results:
+        print(event["event"])
+        print(event["label"])
+        print(event["date"])
+        print(event["summary"])
+        if 'part of' in event:
+            print(event["part of"])
+
+        #     print 200 chracters either side of the extra search terms and ask if it is relevant, if it is, append to outputs as a tuple (event, sentences with search terms
+
+        for term in extra_search_terms:
+            if term in event["summary"].lower():
+                index = event["summary"].lower().index(term)
+                print("\n:::")
+                print(event["summary"][index-200:index+200])
+                print("Is this relevant? (y/n)")
+                if input() == 'y':
+                    outputs.append((event['event'], event["summary"][index-200:index+200]))
+                    break
+
+        print("\n")
+
+    for output in outputs:
+        output_file.write(output[0] + " # " + output[1] + "\n")
+    output_file.close()
+
+
+
 
 if __name__ == "__main__":
     # economic_consequences_spanish_war()
     # destructive_europe_ww1()
     # river_18th_century()
-    portuguese_as_allies()
+    # portuguese_as_allies()
+    revolutions_economic_consequences()
