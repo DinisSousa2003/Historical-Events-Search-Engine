1f50c23a5f378ff6c0d09e53a11f941834a5c6aa
João Matos <jcarvalhaismatos@gmail.com>
2023-11-13 00:55:43 +0000

    changed the schema a bit: added a related_terms list and some custom types. also moved startup.sh

---
+++ solr/startup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This script expects a container started with the following command.
+# docker run -p 8983:8983 --name pri_44 -v ${PWD}:/data -d solr:9.3 solr-precreate conflicts
+#
+
+
+#remove its schema and documents
+docker exec -it pri_44 bin/solr delete -c conflicts
+
+#create core conflicts
+docker exec -it pri_44 bin/solr create -c conflicts
+
+docker exec -it pri_44 cp /data/related_terms.txt /var/solr/data/conflicts/related_terms.txt
+
+
+
+# Schema definition via API
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary "@./schema.json" \
+    http://localhost:8983/solr/conflicts/schema
+
+#,!!! data.json is a copy of the file from /outputs, the relative path was not working
+curl -X POST -H 'Content-type:application/json' --data-binary "@./data.json" http://localhost:8983/solr/conflicts/update?commit=true
+
+# Populate collection using mapped path inside container.
+#docker exec -it pri_44 bin/post -c conflicts ./outputs/data.json
+
+#xdg-open http://localhost:8983/
